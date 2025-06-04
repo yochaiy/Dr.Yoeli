b22273661d60cc56de7ffd2a9b08c1f09e34a6b8
yochaiy <145541951+yochaiy@users.noreply.github.com>
2025-06-04 17:00:09 +0300

    Add files via upload

---
+++ questions3.js
@@ -0,0 +1,202 @@
+const questionsData = [
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+{
+  question:
+    `שאלה`
+    ,
+
+  answer:
+    `תשובה`
+
+},
+
+
+{
+  question:
+   `שאלה לסיום`
+    ,
+
+  answer:
+    `סיום`
+
+}
+
+];
