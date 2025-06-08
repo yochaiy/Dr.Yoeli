2af6d77932a6568fdaa73ed62582b30ed95903de
yochaiy <145541951+yochaiy@users.noreply.github.com>
2025-06-08 23:11:56 +0300

    Add files via upload

---
+++ example.js
@@ -0,0 +1,816 @@
+const questionsData = [
+{
+  question:
+    `
+     מהו השיעור הממוצע של ה-cycle fecundability בקרב נשים עם אי פוריות על רקע בלתי מוסבר שאינן מקבלות טיפול?
+1-4%
+15-20%
+8-11%
+קטן מ-1%
+
+     `
+    ,
+  image: "images/example.jpg",
+  answer:
+  `
+     תשובה – א
+בין 2-4%
+
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+},
+{
+  question:
+    `
+     שאלה
+     `
+    ,
+
+  answer:
+  `
+     תשובה
+     `
+
+}
+]
